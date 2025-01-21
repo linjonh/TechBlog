8f59c406b1908975c3640b2179c49f0c83a8efd2
linjianyou <linjianyou@live.cn>
2025-01-21 18:11:25 +0800

    PWA base 还原

--- _javascript/pwa/app.js
+++ _javascript/pwa/app.js
@@ -5,9 +5,9 @@ if ('serviceWorker' in navigator) {
   const src = new URL(document.currentScript.src);
   const register = src.searchParams.get('register');
   const baseUrl = src.searchParams.get('baseurl');
-
   if (register) {
     const swUrl = `${baseUrl}/sw.min.js`;
+    console.log(`swUrl=${swUrl}`)
     const notification = document.getElementById('notification');
     const btnRefresh = notification.querySelector('.toast-body>button');
     const popupWindow = Toast.getOrCreateInstance(notification);
