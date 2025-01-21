4906588a20def09fb00d6b01c67e3dff6751d64d
linjianyou <linjianyou@live.cn>
2025-01-21 20:13:06 +0800

    PWA js的路径修改/TechBlog/sw.min.js

--- _javascript/pwa/app.js
+++ _javascript/pwa/app.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
   const register = src.searchParams.get('register');
   const baseUrl = src.searchParams.get('baseurl');
   if (register) {
-    const swUrl = `${baseUrl}/TechBlog/sw.min.js`;
+    const swUrl = `./TechBlog/sw.min.js`;
     console.log(`swUrl=${swUrl}`)
     const notification = document.getElementById('notification');
     const btnRefresh = notification.querySelector('.toast-body>button');
