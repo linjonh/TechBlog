0f7213c3ac38505ccbba184ebaeb943b38c88476
linjy <jaysen.dev@gmail.com>
2025-02-19 02:28:58 +0800

    sw 修改header

--- _javascript/pwa/sw.js
+++ _javascript/pwa/sw.js
@@ -66,25 +66,37 @@ self.addEventListener('fetch', (event) => {
     return;
   }
   if (event.request.url.includes('csdnimg.cn')) {
-    event.request.headers.set('Referer', 'null');
-    event.request.headers.set('Origin', 'null');
+    const newHeaders = new Headers(event.request.headers);
+    newHeaders.set('Referer', 'null');
+    newHeaders.set('Origin', 'null');
+
+    const modifiedRequest = new Request(event.request, {
+      headers: newHeaders
+    });
+
+    cacheOrFetch(event, modifiedRequest);
     // 打印headers
     console.log('Request Headers:', [...event.request.headers.entries()]);
 
     //打印referer和origin
     console.log('Referer and Origin headers have been set to null');
+  } else {
+    let request = event.request.clone();
+    cacheOrFetch(event, request);
   }
+});
 
+function cacheOrFetch(event, request) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(request).then((response) => {
       if (response) {
         return response;
       }
 
-      return fetch(event.request).then((response) => {
-        const url = event.request.url;
+      return fetch(request).then((response) => {
+        const url = request.url;
 
-        if (purge || event.request.method !== 'GET' || !verifyUrl(url)) {
+        if (purge || request.method !== 'GET' || !verifyUrl(url)) {
           return response;
         }
 
@@ -92,10 +104,10 @@ self.addEventListener('fetch', (event) => {
         let responseToCache = response.clone();
 
         caches.open(swconf.cacheName).then((cache) => {
-          cache.put(event.request, responseToCache);
+          cache.put(request, responseToCache);
         });
         return response;
       });
     })
   );
-});
+}
