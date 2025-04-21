0730e2164791349c0cb655149823a7c6d2717601
linjianyou <linjianyou@live.cn>
2025-04-21 11:49:20 +0800

    add firebase hosting config. and analytics

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,9 @@
 import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
-
+import { pluginGoogleAnalytics } from 'rsbuild-plugin-google-analytics';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
+  outDir: "out",
   title: 'TechBlog',
   icon: '/rspress-icon.png',
   logo: {
@@ -22,4 +23,10 @@ export default defineConfig({
     enableContentAnimation: true,
     enableAppearanceAnimation: true,
   },
+  builderPlugins: [
+    pluginGoogleAnalytics({
+      // replace this with your Google tag ID
+      id: 'G-QQED2VVKJ3',
+    }),
+  ],
 });
